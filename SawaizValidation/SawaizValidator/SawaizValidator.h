e7273561b6dd28da92eda791baffa228542857a8
Sawaiz Naveed <sawaiz.naveed@10pearls.com>
2023-02-26 15:31:17 +0500

    add frameWork

---
+++ SawaizValidation/SawaizValidator/SawaizValidator.h
@@ -0,0 +1,18 @@
+//
+//  SawaizValidator.h
+//  SawaizValidator
+//
+//  Created by Sawaiz Naveed on 26/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SawaizValidator.
+FOUNDATION_EXPORT double SawaizValidatorVersionNumber;
+
+//! Project version string for SawaizValidator.
+FOUNDATION_EXPORT const unsigned char SawaizValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SawaizValidator/PublicHeader.h>
+
+
---
+++ SawaizValidation/SawaizValidator/ValidateEmail.swift
@@ -0,0 +1,20 @@
+//
+//  ValidateEmail.swift
+//  SawaizValidator
+//
+//  Created by Sawaiz Naveed on 26/02/2023.
+//
+
+import Foundation
+public struct Validator{
+    public static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    public static func addValues(_num1 : Int, _num2 : Int){
+        print("SUM :",_num1 + _num2)
+    }
+}
+
